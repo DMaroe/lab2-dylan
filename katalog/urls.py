763412fafacfa7d8a2516b1004cbe6a7ca9cfbb6
Diablø <macbook@MacBook-Air.local>
2022-09-15 11:16:05 +0700

    Completed Assignment 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import querying
+
+app_name = 'wishlist'
+
+urlpatterns = [
+    path('', querying, name='querying'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def querying(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+    'list_item': data_katalog_item,
+    'name': 'Dylan',
+    'studentid' : '2106720872',
+    }
+    return render(request, "katalog.html", context)
+    
